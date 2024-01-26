ac7777b5a03d04a622fe001c07c990a7c04e2f15
Captain <qq79402005@gmail.com>
2024-01-27 01:47:17 +0800

    extract frame from video #1

--- examples/video-frame-extractor/source/main.cpp
+++ examples/video-frame-extractor/source/main.cpp
@@ -2,8 +2,8 @@
 #include <opencv2/opencv.hpp>
 
 int main() {
-    std::string videoPath = "path/to/video.mp4";
-    std::string outputPath = "path/to/input/";
+    std::string videoPath = "D:/test/video.mp4";
+    std::string outputPath = "D:/test/input/";
 
     cv::VideoCapture videoCapture(videoPath);
 
@@ -13,15 +13,20 @@ int main() {
         return -1;
     }
 
+    int framesCapturePerSecond = 1;
+    double fps = videoCapture.get(cv::CAP_PROP_FPS);
+    int interval = cvRound(fps / framesCapturePerSecond);
+
     int frameCount = 0;
     cv::Mat frame;
 
     while (videoCapture.read(frame)) 
     {
-        std::string outputFilename = outputPath + "frame_" + std::to_string(frameCount) + ".jpg";
-
-        // Save the current frame as an image
-        cv::imwrite(outputFilename, frame);
+        if (frameCount % interval == 0)
+        {
+            std::string outputFilename = outputPath + "frame_" + std::to_string(frameCount) + ".jpg";
+            cv::imwrite(outputFilename, frame);
+        }
 
         frameCount++;
     }
