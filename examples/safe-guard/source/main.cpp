88c2be965a2f08527d233699ed4cbfc172066f02
Captain <qq79402005@gmail.com>
2024-02-05 02:43:26 +0800

    Save mp4

--- examples/safe-guard/source/main.cpp
+++ examples/safe-guard/source/main.cpp
@@ -1,9 +1,47 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <filesystem>
 #include <chrono>
 
-int main() 
+bool detectHuman(cv::Mat& frame)
 {
+    cv::Mat gray, bg, diff, thresh;
+
+    bool motionDetected = false;
+    cv::Ptr<cv::BackgroundSubtractor> bgSubtractor = cv::createBackgroundSubtractorMOG2();
+
+    // TODO: 在这里实现人的动态检测
+    cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+    bgSubtractor->apply(gray, bg);
+
+    cv::absdiff(bg, gray, diff);
+    cv::threshold(diff, thresh, 30, 255, cv::THRESH_BINARY);
+
+    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
+    cv::morphologyEx(thresh, thresh, cv::MORPH_CLOSE, kernel);
+
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(thresh, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    motionDetected = false;
+    for (const auto& contour : contours) {
+        double area = cv::contourArea(contour);
+        if (area > 1000) { // 设置最小面积阈值来确保检测到的对象足够大
+            motionDetected = true;
+            break;
+        }
+    }
+
+    return motionDetected;
+}
+
+int main(int argc, char** argv)
+{
+    // Set current path
+    std::filesystem::path executablePath = std::filesystem::path(argv[0]);
+    std::filesystem::path currentPath = executablePath.parent_path();
+    std::filesystem::current_path(currentPath);
+
     cv::VideoCapture cap(0); // 打开摄像头，0代表默认的摄像头设备
 
     if (!cap.isOpened()) {
@@ -12,8 +50,6 @@ int main()
     }
 
     cv::Mat frame;
-    bool motionDetected = false;
-    cv::Ptr<cv::BackgroundSubtractor> bgSubtractor = cv::createBackgroundSubtractorMOG2();
 
     std::chrono::steady_clock::time_point startTime, endTime;
     int videoDuration = 300; // 5 minutes in seconds
@@ -27,49 +63,28 @@ int main()
             break;
         }
 
-        // TODO: 在这里实现人的动态检测
-        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
-        bgSubtractor->apply(gray, bg);
-
-        cv::absdiff(bg, gray, diff);
-        cv::threshold(diff, thresh, 30, 255, cv::THRESH_BINARY);
-
-        cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
-        cv::morphologyEx(thresh, thresh, cv::MORPH_CLOSE, kernel);
-
-        std::vector<std::vector<cv::Point>> contours;
-        cv::findContours(thresh, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-        motionDetected = false;
-        for (const auto& contour : contours) {
-            double area = cv::contourArea(contour);
-            if (area > 1000) { // 设置最小面积阈值来确保检测到的对象足够大
-                motionDetected = true;
-                break;
-            }
-        }
-
-
-        if (motionDetected) {
-            if (!videoWriter.isOpened()) {
+        bool motionDetected = detectHuman(frame);
+        if (motionDetected) 
+        {
+            if (!videoWriter.isOpened()) 
+            {
                 startTime = std::chrono::steady_clock::now();
-                std::string filename = "video_" + std::to_string(startTime.time_since_epoch().count()) + ".avi";
-                videoWriter.open(filename, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30, cv::Size(frame.cols, frame.rows));
+                std::string filename = "video_" + std::to_string(startTime.time_since_epoch().count()) + ".mp4";
+                videoWriter.open(filename, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 24, cv::Size(frame.cols, frame.rows));
             }
             videoWriter.write(frame);
         }
-        else {
-            if (videoWriter.isOpened()) {
-                endTime = std::chrono::steady_clock::now();
-                std::chrono::duration<double> elapsedSec = endTime - startTime;
-                if (elapsedSec.count() >= videoDuration) {
-                    videoWriter.release();
-                    std::cout << "Video saved: " + std::to_string(startTime.time_since_epoch().count()) + ".avi" << std::endl;
-                }
+
+        if (videoWriter.isOpened())
+        {
+            endTime = std::chrono::steady_clock::now();
+            std::chrono::duration<double> elapsedSec = endTime - startTime;
+            if (elapsedSec.count() >= videoDuration)
+            {
+                videoWriter.release();
+                std::cout << "Video saved: " + std::to_string(startTime.time_since_epoch().count()) + ".mp4" << std::endl;
             }
         }
-
-        // 检查是否退出程序的条件
 
         // 检测到按键ESC，退出循环
         if (cv::waitKey(1) == 27) {
